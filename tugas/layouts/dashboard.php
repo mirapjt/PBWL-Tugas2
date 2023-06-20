64a5451a01b783f15dd8474866cfc82676b507a2
mirapjt <mirapanjaitan1@gmail.com>
2023-06-20 18:52:24 +0700

    Tugas2 Pbwl

---
+++ tugas/layouts/dashboard.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>TUGAS 2 WEB | Web Lanjutan</title>
+	<link rel="stylesheet" href="<?php echo AST; ?>/css/style.css">
+</head>
+
+<body>
+
+	<aside>
+
+		<header>
+			<img src="<?php echo AST; ?>/img/minimarket.jpg" class="brand">
+			<div class="user">Mira Aripin Panjaitan</div>
+		</header>
+
+		<nav>
+			<ul>
+				<li>
+					<a href="<?php echo URL; ?>/dashboard">
+						<img class="icon" src="<?php echo AST; ?>/img/home.png" alt=""> Home
+					</a>
+				</li>
+				<li>
+					<a href="<?php echo URL; ?>/user">
+						<img class="icon" src="<?php echo AST; ?>/img/user.png" alt=""> User
+					</a>
+				</li>
+				<li>
+					<a href="<?php echo URL; ?>/golongan">
+						<img class="icon" src="<?php echo AST; ?>/img/menu.png" alt=""> Golongan
+					</a>
+				</li>
+				<li>
+					<a href="<?php echo URL; ?>/pelanggan">
+						<img class="icon" src="<?php echo AST; ?>/img/pelanggan.jpg" alt=""> Pelanggan
+					</a>
+				</li>
+				<li>
+				<a href="<?php echo URL; ?>/index">
+					<img id="logout" class="icon" src="<?php echo AST; ?>/img/logout.png" alt=""> LogOut
+				</a>
+				</li>
+			</ul>
+		</nav>
+
+	</aside>
+
+	<main>
+		<article>
+			<?php require_once ROOT . "app/views/" . $view . ".php"; ?>
+		</article>
+
+		<footer>
+			Copyright &copy; 2023. Designed by Mira Aripin Panjaitan
+		</footer>
+	</main>
+
+</body>
+
+</html>
